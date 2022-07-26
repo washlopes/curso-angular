15a00013320db34e6169db31fa5f6feddff08008
washlopes <washlopes@gmail.com>
2022-07-26 08:38:28 -0300

    passagem da dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  userName: string = 'Washington'
+  userData = {
+    email: "washloeps@gmail.com",
+    role: "Desenvolvedor"
+  }
   title = 'curso-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponentComponent } from './components/primeiro-component/primeiro-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponentComponent
+    PrimeiroComponentComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule,
